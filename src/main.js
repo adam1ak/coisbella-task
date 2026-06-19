76512e56cdece7ce5edbb37170251abebc0d4186
filip <f.admiakk@gmail.com>
2026-06-19 07:45:47 +0200

    feat: refactor init

--- src/main.js
+++ src/main.js
@@ -4,15 +4,29 @@ import { fetchProducts } from './api.js'
 console.log("Main js loaded")
 
 async function init() {
+    const loader = document.getElementById("loader")
+    const errorMessage = document.getElementById("error-message")
+
+    if (!loader || !errorMessage) {
+        console.error("Missing DOM elements ('loader' or 'error-message')")
+        return
+    }
+
     try {
+        loader.classList.remove("hidden")
+        errorMessage.classList.add("hidden")
+
         const products = await fetchProducts()
-        
+
         console.log("Products fetched successfully: ")
         console.log(products)
+
+        loader.classList.add("hidden")
     } catch (error) {
         console.log("Initialization failed in main.js: ", error)
 
-        // user error display
+        loader.classList.add("hidden")
+        errorMessage.classList.remove("hidden")
     }
 }
 
